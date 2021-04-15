3def9c37787e8cd425f6558bc0540e9cce49e0b1
Federico <federicofigini90@gmail.com>
2021-04-15 16:51:29 +0200

    finito

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,21 @@
 var text = document.getElementById("text")
 
-document.getElementById('button-one').addEventListener('click', function(e) {
-    text.className = ""
-    text.classList.add("text-blue")
-});
-document.getElementById('button-two').addEventListener('click', function(e) {
-    text.className = ""
-    text.classList.add("text-red")
-});
-document.getElementById('button-three').addEventListener('click', function(e) {
-    text.className = ""
-    text.classList.add("text-green")
-});
+function changeColor(color) {
+    switch (color) {
+        case "blue":
+            text.className = "";
+            text.classList.add("text-blue");
+            break;
+        case "red":
+            text.className = "";
+            text.classList.add("text-red");
+            break;
+        case "green":
+            text.className = "";
+            text.classList.add("text-green");
+            break;
+        default:
+            break;
+    }
+
+}
